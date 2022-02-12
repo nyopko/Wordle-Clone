1967b60acb4263c9822f4b63936c2fc771d47235
nyopko <44181737+nyopko@users.noreply.github.com>
2022-02-12 16:48:56 -0500

    Added basic game logic that checks if letter is included or not.

--- src/Components/PlayArea/index.js
+++ src/Components/PlayArea/index.js
@@ -4,149 +4,160 @@ import './style.css';
 
 class PlayArea extends React.Component {
     state = {
-      count: 1,
-      entry: "",
-      guess1: "",
-      guess2: "",
-      guess3: "",
-      guess4: "",
-      guess5: "",
-      guess6: "",
-      guesses: [],
-      answer: "beers",
-      lettersUsed: [],
+        count: 1,
+        entry: "",
+        guess1: "",
+        guess2: "",
+        guess3: "",
+        guess4: "",
+        guess5: "",
+        guess6: "",
+        guesses: [],
+        answer: "beers",
+        lettersUsed: [],
+        winArr: [2, 2, 2, 2, 2],
     };
-  
+
     onChange = e => {
-      this.setState({ entry: e.target.value });
+        this.setState({ entry: e.target.value });
     };
-  
+
     handleGuessSubmit = event => {
-      event.preventDefault();
-      // Win Condition
-      if(this.state.entry === this.state.answer){
-        alert("you win");
-      }
-      else{
-        if(!this.state.guesses.includes(this.state.entry)){
-            //   Guess 1
-            if(this.state.count === 1){
-                this.state.guess1 = this.state.entry;
-                this.state.guesses.push(this.state.entry);
-                console.log("guess 1:",this.state.guess1);
-              }
-            //   Guess 2
-              if((this.state.count === 2)){
-                  this.state.guess2 = this.state.entry;
-                  this.state.guesses.push(this.state.entry);
-                  console.log("guess 2:",this.state.guess2);
-              }
-            //   Guess 3  
-              if(this.state.count === 3){
-                  this.state.guess3 = this.state.entry;
-                  this.state.guesses.push(this.state.entry);
-                  console.log("guess 3:",this.state.guess3);
-              }
-            //   Guess 4
-              if(this.state.count === 4){
-                  this.state.guess4 = this.state.entry;
-                  this.state.guesses.push(this.state.entry);
-                  console.log("guess 4:",this.state.guess4);
-              }
-            //   Guess 5  
-              if(this.state.count === 5){
-                  this.state.guess5 = this.state.entry;
-                  this.state.guesses.push(this.state.entry);
-                  console.log("guess 5:",this.state.guess5);
-              }
-            //   Guess 6  
-              if(this.state.count === 6){
-                  this.state.guess6 = this.state.entry;
-                  this.state.guesses.push(this.state.entry);
-                  console.log("guess 6:",this.state.guess6);
-                  alert("You Lose. Game over.")
-              }
-    
-              console.log(this.state.guesses);
-            //   // Fill Letter Graveyard
+        event.preventDefault();
+        // Win Condition
+        if (this.state.entry === this.state.answer) {
+            alert("you win");
+        }
+        else {
+            if (!this.state.guesses.includes(this.state.entry)) {
+                //   Guess 1
+                if (this.state.count === 1) {
+                    this.state.guess1 = this.state.entry;
+                    this.state.guesses.push(this.state.entry);
+                    console.log("guess 1:", this.state.guess1);
+                }
+                //   Guess 2
+                if ((this.state.count === 2)) {
+                    this.state.guess2 = this.state.entry;
+                    this.state.guesses.push(this.state.entry);
+                    console.log("guess 2:", this.state.guess2);
+                }
+                //   Guess 3  
+                if (this.state.count === 3) {
+                    this.state.guess3 = this.state.entry;
+                    this.state.guesses.push(this.state.entry);
+                    console.log("guess 3:", this.state.guess3);
+                }
+                //   Guess 4
+                if (this.state.count === 4) {
+                    this.state.guess4 = this.state.entry;
+                    this.state.guesses.push(this.state.entry);
+                    console.log("guess 4:", this.state.guess4);
+                }
+                //   Guess 5  
+                if (this.state.count === 5) {
+                    this.state.guess5 = this.state.entry;
+                    this.state.guesses.push(this.state.entry);
+                    console.log("guess 5:", this.state.guess5);
+                }
+                //   Guess 6  
+                if (this.state.count === 6) {
+                    this.state.guess6 = this.state.entry;
+                    this.state.guesses.push(this.state.entry);
+                    console.log("guess 6:", this.state.guess6);
+                    alert("You Lose. Game over.")
+                }
 
-            //   let entryArr = this.state.entry.split("");
+                console.log(this.state.guesses);
 
-            //   // CHeck if letter has been used then add it to array if not
+                console.log(this.state.lettersUsed);
+                this.state.count++;
 
-            //   for(let letter of entryArr){
-            //       if(!this.state.lettersUsed.includes(letter)){
-            //           this.state.lettersUsed.push(letter);
-            //       }
-            //   }
+                // Fill graveyard
+                this.fillGraveYard();
 
-              // Combine all letters used to display
+                // Game Logic
 
-              console.log(this.state.lettersUsed);
-              this.state.count++;
-              this.fillGraveYard();
-            //   event.target.reset();
-    
-          }
-          else{
-              alert("Duplicate Entry Please Try Again");
-          }
+                this.gameLogic();
+                //   event.target.reset();
+
+            }
+            else {
+                alert("Duplicate Entry Please Try Again");
+            }
 
         }
     };
-    fillGraveYard(){
-        // Fill Letter Graveyard
+    // Fill Letter Graveyard
+    fillGraveYard() {
+
+        // Split Entry into arr of Letters
 
         let entryArr = this.state.entry.split("");
 
         // CHeck if letter has been used then add it to array if not
 
-        for(let letter of entryArr){
-            if(!this.state.lettersUsed.includes(letter)){
+        for (let letter of entryArr) {
+            if (!this.state.lettersUsed.includes(letter)) {
                 this.state.lettersUsed.push(letter);
             }
         }
 
     }
-  
-    render() {
-      return (
-          <div>
-        <Container>
-            <Row>
-                <Col>
-                <div className='letter-graveyard'>
-                <h3>Graveyard</h3>
-                <h3>{this.state.lettersUsed.join(",")}</h3>
-                </div>
-                </Col>
-                <Col>
-                <form onSubmit={this.handleGuessSubmit}>
-              <h4>{this.state.guess1}</h4>
-              <h4>{this.state.guess2}</h4>
-              <h4>{this.state.guess3}</h4>
-              <h4>{this.state.guess4}</h4>
-              <h4>{this.state.guess5}</h4>
-              <h4>{this.state.guess6}</h4>
-              <h3>{this.state.entry}</h3>
-          <label>
-            <input
-              maxLength="5"
-              placeholder=""
-              value={this.state.entry}
-              onChange={this.onChange}
-            />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-                </Col>
-                <Col>
-                </Col>
-            </Row>
-        </Container>
-        </div>
-      );
+
+    gameLogic() {
+        let scoreArr = [];
+        let entryArr = this.state.entry.split("");
+        let answerArr = this.state.answer.split("");
+
+        for(let letter of entryArr){
+            if(answerArr.includes(letter)){
+                console.log(letter, "is included");
+            }
+            else{
+                console.log(letter, "NOT INCLUDED");
+            }
+        }
     }
-  }
+
+    render() {
+        return (
+            <div>
+                <Container>
+                    <Row>
+                        <Col>
+                            <div className='letter-graveyard'>
+                                <h3>Graveyard</h3>
+                                <h3>{this.state.lettersUsed.join(",")}</h3>
+                            </div>
+                        </Col>
+                        <Col>
+                            <form onSubmit={this.handleGuessSubmit}>
+                                <h4>{this.state.guess1}</h4>
+                                <h4>{this.state.guess2}</h4>
+                                <h4>{this.state.guess3}</h4>
+                                <h4>{this.state.guess4}</h4>
+                                <h4>{this.state.guess5}</h4>
+                                <h4>{this.state.guess6}</h4>
+                                <h3>{this.state.entry}</h3>
+                                <label>
+                                    <input
+                                        maxLength="5"
+                                        placeholder=""
+                                        value={this.state.entry}
+                                        onChange={this.onChange}
+                                    />
+                                </label>
+                                <input type="submit" value="Submit" />
+                            </form>
+                        </Col>
+                        <Col>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        );
+    }
+}
 
 export default PlayArea;

a82feb047350420df8f1de25007e938fd9d0f3bd
nyopko <44181737+nyopko@users.noreply.github.com>
2022-02-12 16:19:11 -0500

    Added in win condition logic, moved the entry to below the guessed words for easier reading.

--- src/Components/PlayArea/index.js
+++ src/Components/PlayArea/index.js
@@ -13,6 +13,7 @@ class PlayArea extends React.Component {
       guess5: "",
       guess6: "",
       guesses: [],
+      answer: "beers",
     };
   
     onChange = e => {
@@ -21,58 +22,71 @@ class PlayArea extends React.Component {
   
     handleGuessSubmit = event => {
       event.preventDefault();
-      if(!this.state.guesses.includes(this.state.entry)){
-        if(this.state.count === 1){
-            this.state.guess1 = this.state.entry;
-            this.state.guesses.push(this.state.entry);
-            console.log("guess 1:",this.state.guess1);
-          }
-          if((this.state.count === 2)){
-              this.state.guess2 = this.state.entry;
-              this.state.guesses.push(this.state.entry);
-              console.log("guess 2:",this.state.guess2);
-          }
-          if(this.state.count === 3){
-              this.state.guess3 = this.state.entry;
-              this.state.guesses.push(this.state.entry);
-              console.log("guess 3:",this.state.guess3);
-          }
-          if(this.state.count === 4){
-              this.state.guess4 = this.state.entry;
-              this.state.guesses.push(this.state.entry);
-              console.log("guess 4:",this.state.guess4);
-          }
-          if(this.state.count === 5){
-              this.state.guess5 = this.state.entry;
-              this.state.guesses.push(this.state.entry);
-              console.log("guess 5:",this.state.guess5);
-          }
-          if(this.state.count === 6){
-              this.state.guess6 = this.state.entry;
-              this.state.guesses.push(this.state.entry);
-              console.log("guess 6:",this.state.guess6);
-          }
-
-          console.log(this.state.guesses);
-          this.state.count++;
-        //   event.target.reset();
-
+      // Win Condition
+      if(this.state.entry === this.state.answer){
+        alert("you win");
       }
       else{
-          alert("Duplicate Entry Please Try Again");
+        if(!this.state.guesses.includes(this.state.entry)){
+            //   Guess 1
+            if(this.state.count === 1){
+                this.state.guess1 = this.state.entry;
+                this.state.guesses.push(this.state.entry);
+                console.log("guess 1:",this.state.guess1);
+              }
+            //   Guess 2
+              if((this.state.count === 2)){
+                  this.state.guess2 = this.state.entry;
+                  this.state.guesses.push(this.state.entry);
+                  console.log("guess 2:",this.state.guess2);
+              }
+            //   Guess 3  
+              if(this.state.count === 3){
+                  this.state.guess3 = this.state.entry;
+                  this.state.guesses.push(this.state.entry);
+                  console.log("guess 3:",this.state.guess3);
+              }
+            //   Guess 4
+              if(this.state.count === 4){
+                  this.state.guess4 = this.state.entry;
+                  this.state.guesses.push(this.state.entry);
+                  console.log("guess 4:",this.state.guess4);
+              }
+            //   Guess 5  
+              if(this.state.count === 5){
+                  this.state.guess5 = this.state.entry;
+                  this.state.guesses.push(this.state.entry);
+                  console.log("guess 5:",this.state.guess5);
+              }
+            //   Guess 6  
+              if(this.state.count === 6){
+                  this.state.guess6 = this.state.entry;
+                  this.state.guesses.push(this.state.entry);
+                  console.log("guess 6:",this.state.guess6);
+                  alert("You Lose. Game over.")
+              }
+    
+              console.log(this.state.guesses);
+              this.state.count++;
+            //   event.target.reset();
+    
+          }
+          else{
+              alert("Duplicate Entry Please Try Again");
+          }
       }
     };
   
     render() {
       return (
         <form onSubmit={this.handleGuessSubmit}>
-              <h3>{this.state.entry}</h3>
               <h4>{this.state.guess1}</h4>
               <h4>{this.state.guess2}</h4>
               <h4>{this.state.guess3}</h4>
               <h4>{this.state.guess4}</h4>
               <h4>{this.state.guess5}</h4>
               <h4>{this.state.guess6}</h4>
+              <h3>{this.state.entry}</h3>
           <label>
             <input
               maxLength="5"

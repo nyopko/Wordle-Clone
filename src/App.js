16f503f3ffbae3c7e4686e40be1a6eee17c1db82
nyopko <44181737+nyopko@users.noreply.github.com>
2022-02-12 14:05:05 -0500

    Added title and react-grid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { Container, Row, Col } from 'react-grid';
+
+import Title from "./Components/Title/index.js"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row>
+          <Col md><Title /></Col>
+        </Row>
+      </Container>
     </div>
   );
 }
---
+++ src/Components/Title/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-grid';
+import './style.css';
+
+function Header() {
+    return (
+        <div id="headerText">
+            <Container>
+                <Row>
+                    <Col sm><h1 className="text">Yopkodle</h1></Col>
+                </Row>
+            </Container>
+        </div>
+    );
+}
+
+export default Header;
